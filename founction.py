e96df4fa53f061bf9aa7ca5af06c8f7fb69e8422
Abbos <abbosqambarov47@gmail.com>
2023-12-23 13:01:57 +0500

    listda berilgan isimlarni yoshi bo'yicha kamayish tartibida saralash

--- founction.py
+++ founction.py
@@ -5,3 +5,9 @@ def ekub(a,b):
         else:
             b=b-a
     print(a)
+def royxat(ls:list):
+    ls.sort(key= lambda x: x[2], reverse=True)
+    print(ls)
+ekub(12,28)
+ls=[("diyor",12),("siroj",48),("baxtiyor",24),("abbos",22),("johongir",23),("og'abek",19)]
+royxat(ls)
