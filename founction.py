7dd7d2b0582080810e302e15872bd4be507f9b79
Abbos <abbosqambarov47@gmail.com>
2023-12-23 13:10:51 +0500

    berilgan str orasidan sonlarni chiqaradi

--- founction.py
+++ founction.py
@@ -8,6 +8,12 @@ def ekub(a,b):
 def royxat(ls:list):
     ls.sort(key= lambda x: x[2], reverse=True)
     print(ls)
+def sonlar(st:str):
+    for x in st:
+        if x.isdigit():
+            print(x,end=" ")
+
 ekub(12,28)
 ls=[("diyor",12),("siroj",48),("baxtiyor",24),("abbos",22),("johongir",23),("og'abek",19)]
 royxat(ls)
+sonlar(st="adss123sf4eddf5dds4w342wd0c")
