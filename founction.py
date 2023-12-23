8a4763702ec6b392b93b64482d9141b48bfa5df5
Abbos <abbosqambarov47@gmail.com>
2023-12-23 14:06:10 +0500

    berilgan dictdan yoshlarini ajratib olish

--- founction.py
+++ founction.py
@@ -22,11 +22,18 @@ def isimlar_royxati(dic:dict):
         x=x.split(":")[0]
         ls.append(x)
     print(ls)
+def yoshlar_royxati(dic:dict):
+    ls=[]
+    for x in dic:
+        y=x.split(":")[1]
+        ls.append(y)
+    print(ls)
 ekub(12,28)
 ls=[("diyor",12),("siroj",48),("baxtiyor",24),("abbos",22),("johongir",23),("og'abek",19)]
 royxat(ls)
 sonlar(st="adss123sf4eddf5dds4w342wd0c")
 ls1=[12,343,21,4,65,89,0,21]
 kvadrat(ls)
-dic={"diyor":12,"siroj":48,"baxtiyor":24,"abbos":22,"johongir":23,"og'abek":19}
+dic={"diyor": 12,"siroj": 48,"baxtiyor": 24,"abbos": 22,"johongir": 23,"og'abek": 19}
 isimlar_royxati(dic)
+yoshlar_royxati(dic)
